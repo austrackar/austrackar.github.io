c65f7167eaa6248ca829b7c07e4edfe745346361
lucasbotter23-cpu <lucasbotter23@gmail.com>
2026-05-17 21:47:29 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -13,19 +13,19 @@ document.addEventListener('DOMContentLoaded', () => {
   buildAlertsList('all');
   buildLegendModal();
   setupEventListeners();
-  registerServiceWorker();
+  //registerServiceWorker();
   checkOnlineStatus();
   setTimeout(startNotificationDemo, 3000);
 });
 
 // ─── SERVICE WORKER ─────────────────────────────
-function registerServiceWorker() {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./service-worker.js')
-      .then(reg => console.log('SW registrado:', reg.scope))
-      .catch(err => console.log('SW error:', err));
-  }
-}
+//function registerServiceWorker() {
+  //if ('serviceWorker' in navigator) {
+    //navigator.serviceWorker.register('./service-worker.js')
+      //.then(reg => console.log('SW registrado:', reg.scope))
+      //.catch(err => console.log('SW error:', err));
+  //}
+//}
 
 // ─── ONLINE/OFFLINE ─────────────────────────────
 function checkOnlineStatus() {
